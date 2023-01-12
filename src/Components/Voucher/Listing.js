af9e2e67c0c1697b0b50eb3dd58f080b54f4f5b5
ganeshtharol <ganesh@emizentech.com>
2023-01-12 16:10:16 +0530

    add static pagination on listing page

--- src/Components/Voucher/Listing.js
+++ src/Components/Voucher/Listing.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+import Pagination from 'react-bootstrap/Pagination';
+
 const Listing = () => {
+
+
+    let active = 2;
+    let items = [];
+    for (let number = 1; number <= 5; number++) {
+    items.push(
+        <Pagination.Item key={number} active={number === active}>
+        {number}
+        </Pagination.Item>,
+    );
+    }
   
     return (
       <>
@@ -34,6 +47,11 @@ const Listing = () => {
                           })
                       }
                   </div>
+                  <Pagination>
+                        <Pagination.Prev />   
+                        {items}
+                        <Pagination.Next />
+                    </Pagination>
               </div>
           </section>
       </div>
